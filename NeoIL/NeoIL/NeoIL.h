5f63dc36cc11fde4590387d945be3f38b86f7cc9
datacrystals <tliao@carboncopies.org>
2022-08-26 09:00:10 +0000

    Add Header To NeoIL

--- NeoIL/NeoIL/NeoIL.h
+++ NeoIL/NeoIL/NeoIL.h
@@ -2,3 +2,33 @@
 // New Image Library (NeoIL) //
 // Thomas Liao (2022)        //
 //---------------------------//
+
+
+
+namespace NeoIL {
+
+class NeoIL {
+
+private:
+
+    
+
+public:
+
+    /**
+     * @brief Create new NeoIL instance, automatically initializes all required image loader functions.
+     * 
+     */
+    NeoIL();
+
+    /**
+     * @brief Shut down and destroy the NeoIL Instance.
+     * 
+     */
+    ~NeoIL();
+
+
+
+};
+
+};
