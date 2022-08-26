4ef6f141ab5baf08c7ba97b09949379395e92a27
datacrystals <tliao@carboncopies.org>
2022-08-26 08:29:32 +0000

    Setup Build System

---
+++ Tests/CompileTest/Build/CMakeFiles/NeoIL-CompileTest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../Binaries/NeoIL-CompileTest"
+  "../Binaries/NeoIL-CompileTest.pdb"
+  "CMakeFiles/NeoIL-CompileTest.dir/Main.cpp.o"
+  "CMakeFiles/NeoIL-CompileTest.dir/Main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NeoIL-CompileTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
