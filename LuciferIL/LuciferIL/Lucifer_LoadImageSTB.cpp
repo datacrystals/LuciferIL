6e91196926b64acb2173670ae167611c803eae2f
datacrystals <tliao@carboncopies.org>
2022-08-26 22:13:51 +0000

    Update Gitignore

--- LuciferIL/LuciferIL/Lucifer_LoadImageSTB.cpp
+++ LuciferIL/LuciferIL/Lucifer_LoadImageSTB.cpp
@@ -10,16 +10,19 @@ namespace Lucifer {
 
 LoadingStatus Lucifer_STBLoad(unsigned char* ImageData, unsigned long Size, Image& Image, int MaxChannels) {
 
+    // Sanity Check On Input
     if (ImageData == nullptr) {
         return Lucifer_LoadingStatus_InvalidData;
     }
 
+    // Decode Image In Memory
     int Width, Height, Channels;
     unsigned char* ImageBytes = stbi_load_from_memory(ImageData, Size, &Width, &Height, &Channels, 0);
     if (ImageBytes == nullptr) {
         return Lucifer_LoadingStatus_UnsupportedFormat;
     }
 
+    // Copy Image Data
     unsigned long ImageLength = strlen((const char*)ImageBytes);
     Image.Bytes.reset(new unsigned char[ImageLength]);
     memcpy(Image.Bytes.get(), ImageBytes, ImageLength);
@@ -30,7 +33,7 @@ LoadingStatus Lucifer_STBLoad(unsigned char* ImageData, unsigned long Size, Imag
 
     stbi_image_free(ImageBytes);
     
-
+    // More Sanity Checking
     if (Image.Width < 1) {
         return Lucifer_LoadingStatus_InvalidWidth;
     } else if (Image.Height < 1) {
