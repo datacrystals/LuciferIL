6e1e8fab7927c820fb8b6f45b6f048d5edfa3ef2
datacrystals <tliao@carboncopies.org>
2022-08-26 23:50:19 +0000

    Create Files For Image Writer

---
+++ LuciferIL/LuciferIL/Lucifer_WriteImage.cpp
@@ -0,0 +1,11 @@
+//-----------------------//
+// Lucifer Image Library //
+// Thomas Liao (2022)    //
+//-----------------------//
+
+#include <Lucifer_WriteImage.h>
+
+namespace Lucifer {
+
+
+}
---
+++ LuciferIL/LuciferIL/Lucifer_WriteImage.h
@@ -0,0 +1,26 @@
+//-----------------------//
+// Lucifer Image Library //
+// Thomas Liao (2022)    //
+//-----------------------//
+
+#pragma once
+
+
+// Standard Libs
+#include <iostream>
+#include <memory>
+#include <cstring>
+
+// Internal Libs
+
+#include <LuciferIL/Lucifer_LoadingStatusEnum.h>
+#include <LuciferIL/Lucifer_ImageStruct.h>
+
+
+namespace Lucifer {
+
+
+
+
+
+};
