d59fc4360ea38cf0f8ed46f0c6d6e8b5ee7b0b13
datacrystals <tliao@carboncopies.org>
2022-08-26 19:35:57 +0000

    Fix Dep Warnings

--- NeoIL/NeoIL/NeoIL.cpp
+++ NeoIL/NeoIL/NeoIL.cpp
@@ -3,3 +3,16 @@
 // Thomas Liao (2022)        //
 //---------------------------//
 
+#include <NeoIL.h>
+
+NeoIL::NeoIL::NeoIL() {
+
+    FreeImage_Initialise();
+
+}
+
+NeoIL::NeoIL::~NeoIL() {
+
+    FreeImage_DeInitialise();
+
+}
--- NeoIL/NeoIL/NeoIL.h
+++ NeoIL/NeoIL/NeoIL.h
@@ -38,8 +38,6 @@ public:
     ~NeoIL();
 
 
-
-
 };
 
 
