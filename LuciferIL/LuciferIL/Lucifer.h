c411945c83f78b6cbeb4059a2a36afe6b0948824
datacrystals <tliao@carboncopies.org>
2022-08-26 22:19:50 +0000

    Restructure Image Loaders

--- LuciferIL/LuciferIL/Lucifer.h
+++ LuciferIL/LuciferIL/Lucifer.h
@@ -9,8 +9,8 @@
 #include <iostream>
 
 // Internal Lib
-#include <Lucifer_LoadImageSTB.h>
 #include <Lucifer_LoadImageFreeImage.h>
+#include <Lucifer_LoadImageSTB.h>
 
 // Third-Party Lib
 #define STB_IMAGE_IMPLEMENTATION
