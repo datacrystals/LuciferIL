f1af73ebff2e571536db16af1453aa0067174be5
datacrystals <tliao@carboncopies.org>
2022-08-26 21:09:19 +0000

    Create Loading Test

--- NeoIL/NeoIL/NeoIL.h
+++ NeoIL/NeoIL/NeoIL.h
@@ -7,8 +7,9 @@
 
 #include <iostream>
 
-
-//#define STB_IMAGE_IMPLEMENTATION
+#ifndef STB_IMAGE_IMPLEMENTATION
+    #define STB_IMAGE_IMPLEMENTATION
+#endif
 #include <stb_image.h>
 
 #include <FreeImage.h>
