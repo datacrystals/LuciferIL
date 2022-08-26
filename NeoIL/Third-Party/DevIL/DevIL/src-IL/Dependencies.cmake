21d9a8d5943e11c89033d2d5311f0d615558afae
datacrystals <tliao@carboncopies.org>
2022-08-26 10:42:48 +0000

    Satisfy Deps For DevIL

--- NeoIL/Third-Party/DevIL/DevIL/src-IL/Dependencies.cmake
+++ NeoIL/Third-Party/DevIL/DevIL/src-IL/Dependencies.cmake
@@ -4,7 +4,7 @@ message("Setting Up LibPNG For DevIL")
 include_directories(${CMAKE_CURRENT_SOURCE_DIR}/Third-Party/LibPNG)
 add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/Third-Party/LibPNG)
 
-target_include_directories(png PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/Third-Party/LibPNG)
+#target_include_directories(png PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/Third-Party/LibPNG)
 
 # set(DEVIL_DEP_FILES
 #     Third-Party/LibPNG/png.c
