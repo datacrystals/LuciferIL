ced56acb579d3671638dc87da0ff3189ddbb01c9
datacrystals <tliao@carboncopies.org>
2022-08-26 22:14:55 +0000

    Update Gitignore

--- LuciferIL/LuciferIL/Lucifer_LoadImageFreeImage.cpp
+++ LuciferIL/LuciferIL/Lucifer_LoadImageFreeImage.cpp
@@ -3,7 +3,7 @@
 // Thomas Liao (2022)        //
 //---------------------------//
 
-#include <LuciferIL/Lucifer_FreeImageLoadImage.h>
+#include <LuciferIL/Lucifer_LoadImageFreeImage.h>
 
 namespace Lucifer {
 
