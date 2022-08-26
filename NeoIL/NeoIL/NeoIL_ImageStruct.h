7f2a508452e10f92d65c837ca225c799a84ebc9f
datacrystals <tliao@carboncopies.org>
2022-08-26 09:48:54 +0000

    Implement Image Enum Status

--- NeoIL/NeoIL/NeoIL_ImageStruct.h
+++ NeoIL/NeoIL/NeoIL_ImageStruct.h
@@ -11,6 +11,17 @@ namespace NeoIL {
 
 struct Image {
 
+private:
+
+public:
+
+    int Width  = -1; /**<Width in pixels of the image*/
+    int Height = -1; /**<Height in pixels of the image*/
+    int Channels = -1; /**<Number of channels that the image has (RGB would be 3, RGBA is 4, etc...)*/
+
+    unsigned long Size; /**<Number Of Bytes In Image Data*/
+    unsigned char* ImageData; /**<Pointer to bytes containing the image data*/
+
 };
 
 
