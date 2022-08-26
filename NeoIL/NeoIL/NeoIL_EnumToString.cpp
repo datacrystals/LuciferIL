c1cb5f7c97520cd68fb14a74233c568d531ca5f7
datacrystals <tliao@carboncopies.org>
2022-08-26 09:41:21 +0000

    Implement Image Enum Status

--- NeoIL/NeoIL/NeoIL_EnumToString.cpp
+++ NeoIL/NeoIL/NeoIL_EnumToString.cpp
@@ -4,3 +4,27 @@
 //---------------------------//
 
 #include <NeoIL_EnumToString.h>
+
+
+
+std::string NeoIL::NeoIL_GetLoadingStatusString(NeoIL::LoadingStatus Status) {
+
+    if (Status == NeoIL_LoadingStatus_Complete) {
+        return "NeoIL_LoadingStatus_Complete";
+    } else if (Status == NeoIL_LoadingStatus_InvalidData) {
+        return "NeoIL_LoadingStatus_InvalidData";
+    } else if (Status == NeoIL_LoadingStatus_InvalidHeight) {
+        return "NeoIL_LoadingStatus_InvalidHeight";
+    } else if (Status == NeoIL_LoadingStatus_InvalidNumChannels) {
+        return "NeoIL_LoadingStatus_InvalidNumChannels";
+    } else if (Status == NeoIL_LoadingStatus_InvalidWidth) {
+        return "NeoIL_LoadingStatus_InvalidWidth";
+    } else if (Status == NeoIL_LoadingStatus_UnsupportedFormat) {
+        return "NeoIL_LoadingStatus_UnsupportedFormat";
+    } else if (Status == NeoIL_LoadingStatus_UnknownError) {
+        return "NeoIL_LoadingStatus_UnknownError";
+    }
+
+    return "Enum Case Error";
+
+}
