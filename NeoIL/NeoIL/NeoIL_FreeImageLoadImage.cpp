9f193eb4b3b3cbecdebcee7de04eac14499f4305
datacrystals <tliao@carboncopies.org>
2022-08-26 20:01:10 +0000

    Implement Wrapper

---
+++ NeoIL/NeoIL/NeoIL_FreeImageLoadImage.cpp
@@ -0,0 +1,32 @@
+//---------------------------//
+// New Image Library (NeoIL) //
+// Thomas Liao (2022)        //
+//---------------------------//
+
+#include <NeoIL_STBLoadImage.h>
+
+NeoIL::LoadingStatus NeoIL_STBLoad(unsigned char* ImageData, unsigned long Size, NeoIL::Image& Image, int MaxChannels) {
+
+    if (ImageData == nullptr) {
+        return NeoIL::NeoIL_LoadingStatus_InvalidData;
+    }
+
+    int Width, Height, Channels;
+    unsigned char* ImageBytes = stbi_load_from_memory(ImageData, Size, &Width, &Height, &Channels, 0);
+    Image.Bytes = std::make_unique<unsigned char*>(ImageBytes);
+    Image.Width = Width;
+    Image.Height = Height;
+    Image.Channels = Channels;
+
+    if (Image.Bytes == nullptr) {
+        return NeoIL::NeoIL_LoadingStatus_UnsupportedFormat;
+    } else if (Image.Width < 1) {
+        return NeoIL::NeoIL_LoadingStatus_InvalidWidth;
+    } else if (Image.Height < 1) {
+        return NeoIL::NeoIL_LoadingStatus_InvalidHeight;
+    } else if (Image.Channels < 1 || Image.Channels > MaxChannels) {
+        return NeoIL::NeoIL_LoadingStatus_InvalidNumChannels;
+    }
+    return NeoIL::NeoIL_LoadingStatus_Complete;
+
+}
---
+++ NeoIL/NeoIL/NeoIL_FreeImageLoadImage.h
@@ -0,0 +1,40 @@
+//---------------------------//
+// New Image Library (NeoIL) //
+// Thomas Liao (2022)        //
+//---------------------------//
+
+#pragma once
+
+
+// Standard Libs
+#include <iostream>
+
+// External Libs
+#include <stb_image.h>
+
+#include <FreeImage.h>
+
+// Internal Libs
+#include <NeoIL/NeoIL_LoadingStatusEnum.h>
+#include <NeoIL/NeoIL_ImageStruct.h>
+
+
+namespace NeoIL {
+
+
+/**
+ * @brief Decodes image data in-memory.
+ * 
+ * @param ImageData Pointer to raw bytes (these could be read directly off of the disk)
+ * @param Size Size of the array
+ * @param Image Reference to the image to be updated
+ * @param MaxChannels Maximum number of supported channels, defaults to 4
+ * @return NeoIL::LoadingStatus Status of loading
+ */
+NeoIL::LoadingStatus NeoIL_STBLoad(unsigned char* ImageData, unsigned long Size, NeoIL::Image& Image, int MaxChannels = 4);
+
+
+
+
+
+};
