df6f7dd50817c40b479306723167a63ce5e9fcb0
datacrystals <tliao@carboncopies.org>
2022-08-26 09:33:37 +0000

    Implement Image Enum Status

---
+++ NeoIL/NeoIL/NeoIL_ImageStruct.h
@@ -0,0 +1,17 @@
+//---------------------------//
+// New Image Library (NeoIL) //
+// Thomas Liao (2022)        //
+//---------------------------//
+
+#pragma once
+
+
+namespace NeoIL {
+
+
+struct Image {
+
+};
+
+
+};
