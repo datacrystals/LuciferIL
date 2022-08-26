38a45c9b23ca8f1d96be85891f3a90c09608a28d
datacrystals <tliao@carboncopies.org>
2022-08-26 09:31:39 +0000

    Implement Image Enum Status

--- NeoIL/NeoIL/NeoIL_StatusEnum.h
+++ NeoIL/NeoIL/NeoIL_StatusEnum.h
@@ -4,3 +4,20 @@
 //---------------------------//
 
 #pragma once
+
+
+namespace NeoIL {
+
+
+enum LoadingStatus {
+    NeoIL_ImageLoad_Complete,
+    NeoIL_ImageLoad_InvalidData,
+    NeoIL_ImageLoad_UnsupportedFormat,
+    NeoIL_ImageLoad_InvalidNumChannels,
+    NeoIL_ImageLoad_InvalidWidth,
+    NeoIL_ImageLoad_InvalidHeight, 
+    NeoIL_ImageLoad_UnknownError
+};
+
+
+};
