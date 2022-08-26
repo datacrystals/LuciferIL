a1344a4b3ef8153bc859e851128f82cba8c651ca
datacrystals <tliao@carboncopies.org>
2022-08-26 19:54:41 +0000

    Implement Wrapper

--- NeoIL/NeoIL/NeoIL_ImageStruct.h
+++ NeoIL/NeoIL/NeoIL_ImageStruct.h
@@ -20,7 +20,7 @@ public:
     int Channels = -1; /**<Number of channels that the image has (RGB would be 3, RGBA is 4, etc...)*/
 
     unsigned long  Size;  /**<Number Of Bytes In Image Data*/
-    unsigned char* Bytes; /**<Pointer to bytes containing the image data*/
+    std::unique_ptr<unsigned char> Bytes; /**<Pointer to bytes containing the image data*/
 
 
     /**
