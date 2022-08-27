4f29598ae3714af0994be371c8bf1159e67aa78b
datacrystals <tliao@carboncopies.org>
2022-08-27 01:48:25 +0000

    Update Loader

--- LuciferIL/LuciferIL/Lucifer_LoadImage.cpp
+++ LuciferIL/LuciferIL/Lucifer_LoadImage.cpp
@@ -7,7 +7,7 @@
 
 namespace Lucifer {
 
-LoadingStatus Lucifer_Load(unsigned char* ImageData, unsigned long Size, Image& Image, int MaxChannels) {
+LoadingStatus Lucifer_Load(unsigned char* ImageData, unsigned long &Size, Image& Image, int MaxChannels) {
 
     if (ImageData == nullptr) {
         return Lucifer_LoadingStatus_InvalidData;
