fed9662fd4434edc5bc7b537434c010febfd3e66
datacrystals <tliao@carboncopies.org>
2022-08-27 01:01:39 +0000

    Implement Image Writer

--- LuciferIL/LuciferIL/Lucifer_WriteImage.cpp
+++ LuciferIL/LuciferIL/Lucifer_WriteImage.cpp
@@ -5,6 +5,7 @@
 
 #include <Lucifer_WriteImage.h>
 
+
 namespace Lucifer {
 
 WritingStatus Lucifer_Write(Image& Image, std::unique_ptr<unsigned char> CompressedImageData, unsigned long Size, ImageFormat Format = Lucifer_ImageFormat_PNG) {
