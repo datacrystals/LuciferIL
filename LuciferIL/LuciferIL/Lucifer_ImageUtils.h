ed21ea1918895ce427817a96ec5d21d485c592f3
datacrystals <tliao@carboncopies.org>
2022-08-28 04:18:06 +0000

    Add New Helper Functions

--- LuciferIL/LuciferIL/Lucifer_ImageUtils.h
+++ LuciferIL/LuciferIL/Lucifer_ImageUtils.h
@@ -31,6 +31,23 @@ namespace Lucifer {
 int Lucifer_GetBitsPerPixel(Image &Image);
 
 /**
+ * @brief Returns the width of the image from the struct
+ * 
+ * @param Image Lucifer image struct
+ * @return int Width in pixels of the image
+ */
+int Lucifer_GetWidth(Image &Image);
+
+/**
+ * @brief Returns the height of the image from the struct
+ * 
+ * @param Image Lucifer image struct
+ * @return int Height in pixels of the image
+ */
+int Lucifer_GetHeight(Image &Image);
+int Lucifer_GetChannes(Image &Image)
+
+/**
  * @brief Copies the image into a new FIBITMAP.
  * You'll need to take care of destroying the returned image manually.
  * 
